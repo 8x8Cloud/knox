91b3bf37136c7be68fee4244f34ccba15cedca61
rljohnsn <johnsonrl@yahoo.com>
2020-06-04 21:52:00 -0700

    Bump version: 0.0.22 → 0.0.23

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def read(*names, **kwargs):
 
 setup(
     name='knox',
-    version='0.0.22',
+    version='0.0.23',
     license='Apache-2.0',
     description='Certificate management utilities with Vault backend',
     long_description='%s\n%s' % (
