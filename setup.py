89d389c883530cc45bfc44d7d6e7210d9ec15ef3
rljohnsn <johnsonrl@yahoo.com>
2020-10-21 08:59:46 -0700

    Bump version: 0.1.8 → 0.1.9

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def read(*names, **kwargs):
 
 setup(
     name='knox',
-    version='0.1.8',
+    version='0.1.9',
     license='Apache-2.0',
     description='Certificate management utilities with Vault backend',
     long_description='%s\n%s' % (
--- src/knox/__init__.py
+++ src/knox/__init__.py
@@ -1,3 +1,3 @@
-__version__ = '0.1.8'
+__version__ = '0.1.9'
 from . import cli  # noqa: F401
 from .knox import Knox  # noqa: F401
