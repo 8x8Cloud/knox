fab35fff0c3abf59233d8945371a152c67c6a1ae
rljohnsn <johnsonrl@yahoo.com>
2020-10-24 12:12:16 -0700

    Bump version: 0.1.9 → 0.1.10

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ project = 'knox'
 year = '2020'
 author = 'Lance Johnson'
 copyright = '{0}, {1}'.format(year, author)
-version = release = '0.1.9'
+version = release = '0.1.10'
 
 pygments_style = 'trac'
 templates_path = ['.']
